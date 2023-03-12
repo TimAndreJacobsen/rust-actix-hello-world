01f97a0dc8eb2dc9fb0ea53f31ad54cb56da70e6
Tim Andre Jacobsen <tymmyh@gmail.com>
2023-03-12 11:36:48 +0100

    adds manual route

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,17 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+async fn without_routing_macro() -> impl Responder {
+    HttpResponse::Ok().body("Hello world, without routing macro!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .route("/manual-route", web::get().to(without_routing_macro))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
